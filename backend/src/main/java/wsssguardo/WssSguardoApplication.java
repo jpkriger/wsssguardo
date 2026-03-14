59f52c529e25acec0b70e45d150fecdb3c0dd88d
Lucasdpm <l.pozzo@edu.pucrs.br>
2026-03-13 21:47:54 -0300

    FEAT(backend): adiciona Spring

---
+++ backend/src/main/java/wsssguardo/WssSguardoApplication.java
@@ -0,0 +1,13 @@
+package wsssguardo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WssSguardoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WssSguardoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/wsssguardo/WssSguardoApplicationTests.java
@@ -0,0 +1,13 @@
+package wsssguardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WssSguardoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
