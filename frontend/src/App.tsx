b4241de99de332280b1d0fe77d56fc6a9366eb55
Sandro Costa e Silva <sandro.bcs@hotmail.com>
2026-04-10 22:57:29 -0300

    Arrumei o App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
-import { type ReactElement } from "react";
-import { Outlet } from "react-router";
+import { ReactElement } from "react";
 import { ThemeProvider } from "./components/theme-provider";
+import { Outlet } from "react-router"
 
 export default function App(): ReactElement {
+
   return (
     <ThemeProvider>
       <div className="min-h-screen bg-background transition-colors duration-300 flex flex-col">
@@ -13,6 +14,11 @@ export default function App(): ReactElement {
         <main className="mx-auto max-w-7xl w-full px-6 lg:px-8 py-8 flex-1">
           <Outlet />
         </main>
+
+        <footer className="border-t border-border py-6 transition-colors duration-300">
+          {/* FOOTER GLOBAL VAI AQUI */}
+        </footer>
+        
       </div>
     </ThemeProvider>
   );
