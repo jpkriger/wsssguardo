99db5c7faa645aa981bb0dc2b14b52dfb2119dc0
Sandro Costa e Silva <sandro.bcs@hotmail.com>
2026-04-18 23:20:27 -0300

    FIX(frontend): conserta o footer em telas grandes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,19 +8,21 @@ import GlobalFooter from "./components/GlobalFooter/GlobalFooter";
 export default function App(): ReactElement {
   return (
     <ThemeProvider>
-      <nav>
-        <GlobalHeader />
-      </nav>
+      <div className="flex flex-col min-h-screen w-full">
+        <nav>
+          <GlobalHeader />
+        </nav>
 
-      <div className={styles.container}>
-        <main className={styles.main}>
-          <Outlet />
-        </main>
+        <div className={styles.container}>
+          <main className={styles.main}>
+            <Outlet />
+          </main>
+        </div>
+
+        <footer className="h-22 border-t border-border transition-colors duration-300 mt-auto">
+          <GlobalFooter />
+        </footer>
       </div>
-      <footer className="border-t border-border py-6 transition-colors duration-300">
-        <GlobalFooter />
-      </footer>
-
     </ThemeProvider>
   );
 }
