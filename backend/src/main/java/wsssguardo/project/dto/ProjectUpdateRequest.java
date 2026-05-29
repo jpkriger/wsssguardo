aaeca3098f668812b9c13a8fb0a8ee57e6439d47
Matheus Kur <matheuskur7@gmail.com>
2026-05-28 23:51:55 -0300

    REFACTOR(backend - dto): remove companyID de DTO de update de projeto

--- backend/src/main/java/wsssguardo/project/dto/ProjectUpdateRequest.java
+++ backend/src/main/java/wsssguardo/project/dto/ProjectUpdateRequest.java
@@ -11,8 +11,6 @@ public record ProjectUpdateRequest(
     @Size(max = 255, message = "name must not exceed 255 characters")
     String name,
 
-    UUID companyId,
-
     LocalDate startDate,
     LocalDate endDate,
 
